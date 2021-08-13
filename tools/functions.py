a13afa62c3f01ec16dc2b0d89065e83d85c98d7c
P-T1ps <67214658+P-T1ps@users.noreply.github.com>
2021-08-13 12:09:49 -0400

    Update functions.py

--- tools/functions.py
+++ tools/functions.py
@@ -4,11 +4,11 @@ import requests
 def convert_gbp_usd(amount, round_it=True):
     amount = float(amount)
     conv_url = f"https://www.currency.me.uk/convert/gbp/usd"
-    amt = float(float(requests.get(conv_url).text.split
+    amount = float(float(requests.get(conv_url).text.split
                              ("<input id=\"to\" name=\"to\" value=\"USD\" type=\"hidden\">")[1].split(
         "<input id=\"answer\" name=\"answer\" value=\"")[1].split("\" disabled>")[0]) * amount)
-    amt = round(amt, 2) if round_it else amt
-    return amt
+    amount = round(amount, 2) if round_it else amount
+    return amount
 
 
 def discord_styler(description):
@@ -18,4 +18,6 @@ def discord_styler(description):
     for i in description:
         i = i.split("<p")[1].split("\">")[1].replace("</p>", "")
         a += i + "\n"
+    a = a.replace("&nbsp;", "")
+    a = a.replace("<em>", "").replace("</em>", "")
     return a
