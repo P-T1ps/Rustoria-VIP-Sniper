0c21cf4426eea39fbb16052ac5e9e91258d2951c
P-T1ps <67214658+P-T1ps@users.noreply.github.com>
2021-08-13 12:09:07 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import time
+
 import discord
 from discord.ext import commands
 from discord.ext import tasks
@@ -10,6 +12,7 @@ token = open('token', 'r').read()
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents)
+roles_prefix = "Rustoria "
 
 
 @client.event
@@ -18,140 +21,116 @@ async def on_ready():
     checker.start()
 
 
-@client.command()
-async def usmedium(ctx):
-    role_name = "VIP US Medium"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def uslong(ctx):
-    role_name = "VIP US Long"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def ussmall(ctx):
-    role_name = "VIP US Small"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def usmain(ctx):
-    role_name = "VIP US Main"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def eumedium(ctx):
-    role_name = "VIP EU Medium"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def eulong(ctx):
-    role_name = "VIP EU Long"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def eusmall(ctx):
-    role_name = "VIP EU Small"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@client.command()
-async def eumain(ctx):
-    role_name = "VIP EU Main"
-    user = ctx.author
-    user_roles = ctx.author.roles
-    target_role = discord.utils.get(ctx.message.guild.roles, name=role_name)
-    for role in user_roles:
-        if role.name == role_name:
-            await user.remove_roles(target_role)
-            return
-    await user.add_roles(target_role)
-
-
-@tasks.loop(minutes=1)
-async def checker():
-    picker_channel = client.get_channel(874007831546830898)
-    data = {
-        "us_main": [rustoria_us_main([tools]), "VIP US Main"],
-        "us_medium": [rustoria_us_medium([tools]), "VIP US Medium"],
-        "us_long": [rustoria_us_long([tools]), "VIP US Long"],
-        "us_small": [rustoria_us_small([tools]), "VIP US Small"],
-
-        "eu_main": [rustoria_eu_main([tools]), "VIP EU Main"],
-        "eu_medium": [rustoria_eu_medium([tools]), "VIP EU Medium"],
-        "eu_long": [rustoria_eu_long([tools]), "VIP EU Long"],
-        "eu_small": [rustoria_eu_small([tools]), "VIP EU Small"]
-    }
+@client.event
+async def on_message(message):
+    contents = message.content
+    author = message.author
     guild = client.get_guild(874007431527686154)
-    for x in data:
-        in_stock = dict(data[x][0])["in_stock"]
-        if in_stock:
-            embed = discord.Embed()
-            embed.title = data[x][1]
-            embed.url = "https://donate.rustoria.co/packages.php?game=<GM>&server=<SVR>".replace(
-                "<GM>", dict(data[x][0])['game']).replace("<SVR>", dict(data[x][0])['server'])
-            embed.description = discord_styler(dict(data[x][0])['description'])
-            embed.add_field(name="Price", value="$" + str(dict(data[x][0])['price']), inline=True)
-            role = discord.utils.get(guild.roles, name=data[x][1])
-            for member in guild.members:
-                if role in member.roles:
-                    await member.send(embed=embed,
-                                      content=f"{member.mention}, if you don't buy this in time reassign your role "
-                                              f"in {picker_channel.mention}!")
-                    await member.remove_roles(role)
+    roles = {
+        "usmain": discord.utils.get(guild.roles, id=874013034195066932),
+        "usmedium": discord.utils.get(guild.roles, id=874007919451066458),
+        "uslong": discord.utils.get(guild.roles, id=874013139816054806),
+        "ussmall": discord.utils.get(guild.roles, id=874013079476768819),
+
+        "eumain": discord.utils.get(guild.roles, id=874018564523307018),
+        "eumedium": discord.utils.get(guild.roles, id=874018668344901672),
+        "eulong": discord.utils.get(guild.roles, id=874018735122423830),
+        "eusmall": discord.utils.get(guild.roles, id=874022194257219595)
+    }
+    if contents.startswith("."):
+        contents = contents.split(".", 1)[1]
+        if contents in roles:
+            role = roles[contents]
+            if role in author.roles:
+                await author.remove_roles(role)
+            else:
+                await author.add_roles(role)
+
+
+@tasks.loop(seconds=15)
+async def checker():
+    try:
+        guild = client.get_guild(874007431527686154)
+        roles = {
+            "usmain": discord.utils.get(guild.roles, id=874013034195066932),
+            "usmedium": discord.utils.get(guild.roles, id=874007919451066458),
+            "uslong": discord.utils.get(guild.roles, id=874013139816054806),
+            "ussmall": discord.utils.get(guild.roles, id=874013079476768819),
+
+            "eumain": discord.utils.get(guild.roles, id=874018564523307018),
+            "eumedium": discord.utils.get(guild.roles, id=874018668344901672),
+            "eulong": discord.utils.get(guild.roles, id=874018735122423830),
+            "eusmall": discord.utils.get(guild.roles, id=874022194257219595)
+        }
+        data_functions = {
+            roles["usmain"]: [
+                                [
+                                    rustoria_us_main,
+                                    rustoria_us_main_wipe
+                                ],
+                                discord.utils.get(guild.roles, id=874013034195066932)],
+            roles["usmedium"]: [
+                                [
+                                    rustoria_us_medium
+                                ],
+                                discord.utils.get(guild.roles, id=874007919451066458)],
+            roles["uslong"]: [
+                                [
+                                    rustoria_us_long
+                                ],
+                                discord.utils.get(guild.roles, id=874013139816054806)],
+            roles["ussmall"]: [
+                                [
+                                    rustoria_us_small
+                                ],
+                                discord.utils.get(guild.roles, id=874013079476768819)],
+
+            roles["eumain"]: [
+                                [
+                                    rustoria_eu_main
+                                ],
+                                discord.utils.get(guild.roles, id=874018564523307018)],
+            roles["eumedium"]: [
+                                [
+                                    rustoria_eu_medium
+                                ],
+                                discord.utils.get(guild.roles, id=874018668344901672)],
+            roles["eulong"]: [
+                                [
+                                    rustoria_eu_long
+                                ],
+                                discord.utils.get(guild.roles, id=874018735122423830)],
+            roles["eusmall"]: [
+                                [
+                                    rustoria_eu_small
+                                ],
+                                discord.utils.get(guild.roles, id=874022194257219595)],
+        }
+
+        occupied_roles = [role for role in roles.values() if role.members]
+        for role in occupied_roles:
+            do_break = False
+            for i in data_functions[role][0]:
+                if do_break:
+                    break
+                function_data = i([tools])
+                if function_data["in_stock"]:
+                    do_break = True
+                    for member in role.members:
+                        embed = discord.Embed()
+                        embed.title = function_data['title']
+                        embed.set_image(url=dict(function_data)['img'])
+                        embed.url = "https://donate.rustoria.co/packages.php?game=<GM>&server=<SVR>".replace(
+                            "<GM>", dict(function_data)['game']).replace("<SVR>", dict(function_data)['server'])
+                        embed.description = discord_styler(dict(function_data)['description'])
+                        a = dict(function_data)['price']
+                        price_str = str(a) + "0" if len(str(a).split(".")[1]) == 1 else str(a)
+                        embed.add_field(name="Price", value="$" + price_str, inline=True)
+                        embed.add_field(name="Duration", value=str(int(float(function_data['expires']))) + " Days")
+
+                        await member.send(embed=embed)
+                        await member.remove_roles(role)
+    except Exception as e:
+        print(e, time.time())
 
 client.run(token)
